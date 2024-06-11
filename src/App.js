e17943bd17cd4b63ace85a25958d906d9ba7bed0
Neskines <123345003+neski321@users.noreply.github.com>
2024-06-11 14:04:43 +0000

    Accessibility changes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     <div className="app">
       <Portfolio />
       <footer className="footer">
-        <p>Contact: neskinescharles@gmail.com</p>
-        <p>LinkedIN:  <a href="https://www.linkedin.com/in/neskines-o-5205b3177/" target='_blank' rel="noopener noreferrer" className='linkedin-link'> Let's Connect</a></p>
-        <p>GitHub:  <a href="https://github.com/neski321" target='_blank' rel="noopener noreferrer" className='linkedin-link'> Happy coding!</a></p>
+        <h2>Contact: nmotieno@myseneca.ca</h2>
+        <h3>LinkedIN:  <a href="https://www.linkedin.com/in/neskines-o-5205b3177/" target='_blank' rel="noopener noreferrer" className='linkedin-link'> Let's Connect</a></h3>
+        <h3>GitHub:  <a href="https://github.com/neski321" target='_blank' rel="noopener noreferrer" className='linkedin-link'> Happy coding!</a></h3>
       </footer>
       <Analytics />
     </div>
